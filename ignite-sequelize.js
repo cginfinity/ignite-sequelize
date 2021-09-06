a129286d6ebdc8139de1f26f92ff25b5f4deb65d
RAVI KANT SHARMA <ravikantsharma2169@gmail.com>
2021-09-06 20:40:02 +0530

    updated query nd bind overrides to support parameterized queries

--- ignite-sequelize.js
+++ ignite-sequelize.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const mustache = require('mustache');
-module.exports = function(RED) {
+module.exports = function (RED) {
   'use strict';
   let pgPool = null;
 
@@ -24,9 +24,9 @@ module.exports = function(RED) {
 
   RED.nodes.registerType('database', databaseNode, {
     credentials: {
-      url: {type: 'text'},
-      user: {type: 'text'},
-      password: {type: 'password'}
+      url: { type: 'text' },
+      user: { type: 'text' },
+      password: { type: 'password' }
     }
   });
 
@@ -34,63 +34,62 @@ module.exports = function(RED) {
     const node = this;
     RED.nodes.createNode(this, config);
     node.config = RED.nodes.getNode(config.database);
-    node.on('input', function(msg) {
+    node.on('input', function (msg) {
       var sequelize;
-      if(node.config.url)
-      {
-        sequelize = new Sequelize( node.config.url, {
-          dialect:  node.config.dialect,
+      if (node.config.url) {
+        sequelize = new Sequelize(node.config.url, {
+          dialect: node.config.dialect,
           dialectOptions: {
-            ssl:  node.config.ssl
+            ssl: node.config.ssl
           },
-          logging : false
+          logging: false
         });
       }
-      else{
-        sequelize = new Sequelize( node.config.database, node.config.user,  node.config.password, {
-          host:  node.config.host,
-          port:  node.config.port,
-          dialect:  node.config.dialect,
+      else {
+        sequelize = new Sequelize(node.config.database, node.config.user, node.config.password, {
+          host: node.config.host,
+          port: node.config.port,
+          dialect: node.config.dialect,
           dialectOptions: {
-            ssl:  node.config.ssl
+            ssl: node.config.ssl
           },
-          logging : false
+          logging: false
         });
       }
       var query = "";
-      if(msg.provider === "ignite-odata" && msg.payload[this.config.dialect])
-      {
+      var binds = msg.sBinds || {};
+      if (msg.provider === "ignite-odata" && msg.payload[this.config.dialect]) {
         query = msg.payload[this.config.dialect];
-      }
-      else
-      {
-        if(config.usepayload){
+      } else {
+        if (config.usepayload) {
           query = msg.payload;
-        }
-        else
-        {
+        } else if (msg.sQuery) {
+          query = msg.sQuery;
+        } else {
           query = mustache.render(config.query, {
             msg: msg
           });
         }
       }
-      sequelize.query(query)
-      .then(([results, metadata])  => {
-        msg.payload = results;
-        msg.metadata = metadata;
-        node.send(msg);
-      }, error=>{
-        msg.payload = [];
-        msg.error = error;
-        node.send(msg);
-      });
+      sequelize.query(query, {
+        binds: binds
+      })
+        .then(([results, metadata]) => {
+          msg.payload = results;
+          msg.metadata = metadata;
+          node.send(msg);
+        }, error => {
+          msg.payload = [];
+          msg.error = error;
+          node.send(msg);
+        });
     });
   }
-  RED.nodes.registerType("sequelize",ignitesequelizeNode,{
+  RED.nodes.registerType("sequelize", ignitesequelizeNode, {
     credentials: {
-      url: {type: 'password'},
-      username: {type: 'text'},
-      password: {type: 'password'}
+      url: { type: 'password' },
+      username: { type: 'text' },
+      password: { type: 'password' }
     }
   });
 };
